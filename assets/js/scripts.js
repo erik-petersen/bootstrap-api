5669b0caecfe53c49f6b3b7de958e74081df1592
Erik Petersen <contacterikpetersen@gmail.com>
2016-06-14 23:37:04 -0400

    Added really stupid second API call that has nothing to do with anything except the type of cuisine because I couldn't get the stinking Zillow to work

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,23 +1,60 @@
-var params = {
-  client_id:"JFJKRIE3BWM34PK0MJARBNJ3DAW4WFPQHV1BUC4I0T1NCOR2",
-  client_secret:"URVQR5M1H0STFSZXYOF5EULRQ4EJRATZAXVEK2N3YM402RSR",
-  v:20130815,
-  ll:"40.7,-74",
-  query:"sushi"
-};
+$( function() {
+  "use strict";
 
-$.ajax({
-  dataType: 'json',
-  url: 'https://api.foursquare.com/v2/venues/search',
-  method: 'GET', // This is the default, but I thought I'd show it,
-  data: params
-  })
-  .done(function (data) {
+$( ".btnRestaurant" ).on( "click", function(e) {
+  e.preventDefault();
+  e.stopPropagation();
+  console.log( "So far so good" );
 
-  var restaurantLi = $( ".restaurant" );
-  for( var index = 0; index < 5; index++ ) {
-    console.log(data.response.venues[index].name);
-    $( restaurantLi[index] ).text( data.response.venues[index].name );
-  }
+  var location;
+  var cuisine;
 
-});
+  location = $('.location-choice[name="location"]').val().toString();
+  cuisine = $('.cuisine-choice[name="cuisine"]').val().toString();
+
+  var params = {
+    client_id: "JFJKRIE3BWM34PK0MJARBNJ3DAW4WFPQHV1BUC4I0T1NCOR2",
+    client_secret: "URVQR5M1H0STFSZXYOF5EULRQ4EJRATZAXVEK2N3YM402RSR",
+    v:20130815,
+    ll:"40.7,-74",
+    near: location,
+    query: cuisine
+  };
+
+  console.log(location);
+
+  // begin foursquare GET call
+  $.ajax({
+    dataType: 'json',
+    url: 'https://api.foursquare.com/v2/venues/search',
+    method: 'GET',
+    data: params,
+  }) // end ajax GET request
+    .done(function (data) {
+
+    var restaurantLi = $( ".restaurant" );
+    for( var index = 0; index < 5; index++ ) {
+      console.log(data.response.venues[index].name);
+      $( restaurantLi[index] ).text( data.response.venues[index].name );
+    }
+
+    $(".location-choice").val("");
+    $(".cuisine-choice").val("");
+
+    }); // end done()
+
+    // begin giphy GET call
+    $.ajax({
+      dataType: 'json',
+      url: 'http://api.giphy.com/v1/gifs/search?q=' + cuisine + "&api_key=dc6zaTOxFJmzC",
+      method: 'GET',
+      // data: params,
+    }) // end ajax GET request
+      .done(function (data) {
+
+      console.log(data.data[0].images.fixed_height.url);
+      $( ".house-pic" ).attr( "src", data.data[0].images.fixed_height.url );
+
+      }); // end done()
+  });
+})
